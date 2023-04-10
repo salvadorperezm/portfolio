1a6347c6f7a4055c35fe254ae5f765cd311ea1b1
salvadorperezm <salvadorperezm115@gmail.com>
2023-04-10 13:24:47 -0600

    added vue weather app project

---
+++ src/assets/images/vue-weather-app/index.js
@@ -0,0 +1,20 @@
+import mainMobile from "../vue-weather-app/weather-mobile-main.png";
+import settingsMobile from "../vue-weather-app/weather-mobile-settings.png";
+import modalMobile from "../vue-weather-app/weather-mobile-modal.png";
+
+const vueWeatherAppImages = [
+  {
+    name: "Main mobile",
+    image: mainMobile,
+  },
+  {
+    name: "Settings mobile",
+    image: settingsMobile,
+  },
+  {
+    name: "Modal mobile",
+    image: modalMobile,
+  },
+];
+
+export default vueWeatherAppImages;
